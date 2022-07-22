a183ecfb84bb9a94f48f5d2f2bc56a303a59f977
yousaf wazir <umbrellaman612@gmail.com>
2022-07-22 10:52:04 +0100

    feat: added cinzel font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Cormorant+Unicase:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
