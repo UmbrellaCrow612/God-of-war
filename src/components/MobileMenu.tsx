91156c0f1721812fe9f18b809d8161bc5a257f92
yousaf wazir <umbrellaman612@gmail.com>
2022-07-22 15:33:13 +0100

    code: changed menu to button

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -2,13 +2,13 @@ import { ImOmega } from 'react-icons/im'
 import { useRecoilState } from 'recoil'
 import { mobileDrawerState } from '../../atoms'
 
-import {AiOutlineClose} from "react-icons/ai"
+import { AiOutlineClose } from 'react-icons/ai'
 interface MobileMenuProps {}
 
 export const MobileMenu: React.FC<MobileMenuProps> = () => {
   const [open, setOpen] = useRecoilState(mobileDrawerState)
   return (
-    <div
+    <button
       className="border-[3px] transition-all duration-200 ease-in-out p-3 flex items-center justify-center rounded-full border-[#660000] mobileMenuShadow text-[#F2F3F4] cursor-pointer hover:border-[#203354] hover:scale-110 group focus:border-[#203354] bg-black"
       onClick={() => setOpen(!open)}
     >
@@ -17,6 +17,6 @@ export const MobileMenu: React.FC<MobileMenuProps> = () => {
       ) : (
         <ImOmega className="group-hover:scale-105" />
       )}
-    </div>
+    </button>
   )
 }
