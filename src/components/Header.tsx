b51b4c0e2fad43b6b5abf9cfe569a9bbfb611c94
yousaf wazir <umbrellaman612@gmail.com>
2022-07-22 14:45:13 +0100

    feat: added mobile nav skeleton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import { MobileMenu } from './MobileMenu'
+
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <>
-      <nav>Nav</nav>
+      <nav className="px-4 h-[85px] sticky top-0 flex items-center justify-between border">
+        <MobileMenu />
+        <Link href="/">
+          <a>
+            <div className="width-[120px] h-[100px]">
+              <img
+                src="/images/logo.png"
+                alt="God of war"
+                loading="lazy"
+                className="w-full h-full"
+              />
+            </div>
+          </a>
+        </Link>
+      </nav>
     </>
   )
 }
---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,11 @@
+import { ImOmega } from 'react-icons/im'
+
+interface MobileMenuProps {}
+
+export const MobileMenu: React.FC<MobileMenuProps> = () => {
+  return (
+    <div className="border-[3px] transition-all duration-200 ease-in-out p-3 flex items-center justify-center rounded-full border-[#660000] mobileMenuShadow text-[#F2F3F4] cursor-pointer hover:border-[#203354] hover:scale-110 group focus:border-[#203354] bg-black">
+      <ImOmega className="group-hover:scale-105" />
+    </div>
+  )
+}
